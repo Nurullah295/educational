b2bb6b593bf3ac00fb2b30785886a49f97b8c658
Nurullah295 <mdnourullah@gmail.com>
2026-04-30 23:48:49 +0600

    Add files via upload

---
+++ js/pages-data3.js
@@ -0,0 +1,24 @@
+const pages = [
+    
+{id:	1	,	name:	"Overview of Pharmacology",	url:	"Overview of Pharmacology.html"	},
+{id:	2	,	name:	"Overview of Microbiology",	url:	"Overview of Microbiology.html"	},
+{id:	3	,	name:	"Circulatory system",	url:	"Circulatory system.html"},
+{id:	4	,	name:	"Cardiovascular-npl",	url:	"Cardiovascular-npl.html"},
+{id:	5	,	name:	"Digestive system",	url:	"Digestive system.html"	},
+{id:	6	,	name:	"Endocrine system",	url:	"Endocrine system.html"	},
+{id:	7	,	name:	"Endocrinology & Diabetology_npl",	url:	"Endocrinology & Diabetology_npl.html"	},
+{id:	8	,	name:	"Integumentary system(Skin)",	url:	"Integumentary system(Skin).html"	},
+{id:	9	,	name:	"Lymphatic system",	url:	"Lymphatic system.html"	},
+{id:	10	,	name:	"Muscular System",	url:	"Muscular System.html"	},
+{id:	11	,	name:	"Nervous System",	url:	"Nervous System.html"	},
+{id:	12	,	name:	"Respiratory System",	url:	"Respiratory System.html"	},
+{id:	13	,	name:	"Reproductive System",	url:	"Reproductive System.html"	},
+{id:	14	,	name:	"Skeletal system",	url:	"Skeletal system.html"	},
+{id:	15	,	name:	"Urinary System",	url:	"Urinary System.html"	},
+{id:	16	,	name:	"Sensory Organ(Eye)",	url:	"Sensory Organ(Eye).html"	},
+{id:	17	,	name:	"Disease",	url:	"Disease.html"	},
+{id:	18	,	name:	"Notes",	url:	"Notes.html"	},
+
+
+
+];
